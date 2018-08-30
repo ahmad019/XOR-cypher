4666b25710d4eb102d1a167767deade2bc1781ee
Ahmad Bilal <ahmdbilx@gmail.com>
2018-08-30 22:15:17 +0500

    Uploaded the cipher text

---
+++ xor_cipher.py
@@ -0,0 +1,65 @@
+
+
+def xor(x,y): #function to cypher text
+	g = []
+	for i in range(len(x)):
+		if x[i] == y[i]:
+			g.append(0)
+		elif x[i] != y[i]:
+			g.append(1)
+	return g
+
+#x = [1,0,1,0,1,0,0,1] some key 
+#y = [1,1,1,1,1,0,1,1]     values
+cipher_string = []
+asc_str = []
+asc_list = [[]]
+refined = ([[]])
+cipher = []
+
+
+
+cipher_input = input('Enter the string to cipher: ')
+key_input = input('Enter 8bit key ex:10101101: ')
+
+for ch in cipher_input:
+    
+    cipher_string.append(ch)
+
+num_of_char = len(cipher_string)
+
+for i in range(num_of_char):
+    
+    asc_str.append(('{0:08b}'.format(ord(cipher_string[i]))))
+
+
+jr = 0
+k = 0
+for n in range(len(asc_str)):
+    refined.append([])
+    for object in asc_str[n]:
+        if(jr==8):
+            k += 1
+            jr = 0
+        refined[k].append(object)
+        jr += 1
+        #if(n == len(asc_str)-1):
+         #   break
+        
+        
+l = 0
+
+for l in range(len(refined)):
+    asc_list.append([])
+    asc_list[l] = list(map(int, refined[l]))
+    
+
+key = list(map(int, key_input))
+
+f=0
+
+for f in range(len(asc_list)-2):
+    
+    cipher.append(xor(asc_list[f], key))
+
+print(cipher)
